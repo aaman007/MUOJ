35de18ac8a3cd6d5d2fb494a55817f041be26789
Amanur Rahman <aaman.sunny007@gmail.com>
2020-11-19 19:36:13 +0600

    Fixed files

--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,15 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
 
-# Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    preference = models.BooleanField(default=0)
-    created_at = models.DateTimeField(default=timezone.now)
-    modified_at = models.DateTimeField(default=timezone.now)
+    preference = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
--- problemset/admin.py
+++ problemset/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import language, Problem
 
-# Register your models here.
-admin.site.register(language)
-admin.site.register(Problem)
+from problemset.models import Language, Problem, TestCase, Submission
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TestCase)
+class TestCaseAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    pass
