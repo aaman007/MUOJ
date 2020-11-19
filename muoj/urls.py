d81bea3dd3e754e81c62999000a3e5beb5e31343
Amanur Rahman <aaman.sunny007@gmail.com>
2020-11-19 19:49:42 +0600

    Added base template and /home page

--- muoj/urls.py
+++ muoj/urls.py
@@ -18,7 +18,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='base.html', extra_context={'home': 'active'})),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
