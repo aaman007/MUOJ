a672b558d438af14cb2aa8159e94947b974dfed8
Farhan-meb <farhan.meb@gmail.com>
2020-12-01 00:35:03 +0600

    Added Python judge system

--- problemset/judge.py
+++ problemset/judge.py
@@ -7,7 +7,6 @@ from problemset.models import Submission
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # C++ code compile
 def compile_cpp_submission(submission):
     solution_url = f"{BASE_DIR}{submission.solution.url}"
@@ -49,10 +48,50 @@ def compile_cpp_submission(submission):
     # Update submission status
     Submission.objects.filter(id=submission.id).update(status=result)
 
+# C++ code compile
+def compile_python_submission(submission):
+    solution_url = f"{BASE_DIR}{submission.solution.url}"
+    result = 'AC'
+
+    # Create an executable file for the solution named todo_coder
+    try:
+        subprocess.check_output(f'python {solution_url}', shell=True)
+    except (subprocess.CalledProcessError, Exception):
+        result = 'CE'
+
+    if result == 'AC':
+        for testcase in submission.problem.testcases.all():
+            input_url = f"{BASE_DIR}{testcase.input.url}"
+
+            try:
+                p = subprocess.run(
+                    f'ulimit -t {submission.problem.time_limit}; '
+                    f'ulimit -v {submission.problem.memory_limit*1024}; '
+                    f'python {solution_url} < {input_url} > {BASE_DIR}/media/c++/todo_coder_python_output.txt',
+                    shell=True
+                )
+                if not p.returncode:
+                    with open(f"{BASE_DIR}/media/c++/todo_coder_python_output.txt", 'r', encoding='UTF-8') as f:
+                        participant_output = f.read()
+                        if participant_output != testcase.output_text:
+                            result = 'WA'
+                            break
+                elif p.returncode == 137:
+                    result = 'TLE'
+                elif p.returncode == 139:
+                    result = 'MLE'
+                else:
+                    result = 'RTE'
+            except (FileNotFoundError, Exception):
+                result = 'CE'
+                break
+
+    # Update submission status
+    Submission.objects.filter(id=submission.id).update(status=result)
 
 # Execute submitted file
 def compile_submission(submission):
     language = submission.solution_language
 
-    if language.name == 'C++':
-        compile_cpp_submission(submission)
+    if language.name == 'Python':
+        compile_python_submission(submission)
