0a985c09f53dd346d0ecb980a3e423526706b525
Amanur Rahman <aaman.sunny007@gmail.com>
2021-01-08 20:22:22 +0600

    Added extra fields on Profile and Tutorial model

--- training/models.py
+++ training/models.py
@@ -6,6 +6,7 @@ class Tutorial(models.Model):
     title = models.CharField(verbose_name=_('Title'), max_length=200)
     tutorial = models.TextField(verbose_name=_('Description'), blank=True)
     problem_ids = models.JSONField(verbose_name=_('Problem Ids'), default=list)
+    level = models.PositiveIntegerField(verbose_name=_('Level'), unique=True, default=1)
 
     def __str__(self):
         return self.title
