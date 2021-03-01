c40c20ee649f522312459251055c737bd3891395
Amanur Rahman <aaman.sunny007@gmail.com>
2021-03-02 00:25:37 +0600

    Fixed message list height issue and added svg file to show when page content is empty

--- muoj/settings/production.py
+++ muoj/settings/production.py
@@ -2,4 +2,11 @@ from .staging import *
 
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-DEBUG = False
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db_production.sqlite3',
+    }
+}
