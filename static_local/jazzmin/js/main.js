09f8a0d7b6723254a6b6f464758a75f4f550aa72
Amanur Rahman <aaman.sunny007@gmail.com>
2021-03-15 01:17:21 +0600

    Fixed bugs

--- static_local/jazzmin/js/main.js
+++ static_local/jazzmin/js/main.js
@@ -2,7 +2,12 @@ $(".django-ckeditor-widget").css("display", "block")
 $(".main-footer > .float-right").text("MUOJ version 0.0.1")
 $('.login-logo').html('<h2> MU Online Judge </h2>')
 
-var searchFilters = $('#changelist-search > .form-group');
-searchFilters.each(function () {
-    $(this).css('marginRight', '10px')
-});
+$(document).ready(function () {
+    $('.related-widget-wrapper').find('.select2').each(function () {
+        console.log($(this))
+        $(this).css({
+            maxWidth: "570px",
+            height: "38px"
+        })
+    });
+})
