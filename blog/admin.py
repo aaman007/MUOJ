0ce467be60ef3708c99b5f8e33dbffe23232aa9b
Amanur Rahman <aaman.sunny007@gmail.com>
2021-03-20 11:02:55 +0600

    Fixed Tutorial model ordering issue, added action to mark and unmark preference for blogs in admin dashboard

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
-from .models import Blog, Comment
+from django.utils.translation import gettext_lazy as _
+
+from blog.models import Blog, Comment
 
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ['user', 'title', 'preference']
     list_filter = ['preference']
+    actions = ['mark_preferred', 'unmark_preferred']
+
+    def mark_preferred(self, request, queryset: Blog):
+        queryset.update(preference=True)
+
+    def unmark_preferred(self, request, queryset: Blog):
+        queryset.update(preference=False)
+
+    mark_preferred.short_description = _('Mark selected blogs as preferred')
+    unmark_preferred.short_description = _('Unmark selected blogs as preferred')
 
 
 @admin.register(Comment)
