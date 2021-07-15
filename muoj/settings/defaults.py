17779d617a6fe4b08450eda3eba94d086f792c0b
Md Mahbub Abedin Talukdar <39917360+Farhan-meb@users.noreply.github.com>
2021-07-15 22:14:28 +0600

    Update defaults.py

--- muoj/settings/defaults.py
+++ muoj/settings/defaults.py
@@ -24,6 +24,11 @@ environ.Env.read_env((BASE_DIR / '.env').as_posix())
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('SECRET_KEY')
 
+DEBUG = False
+
+#Hosting in heroku
+ALLOWED_HOSTS = ['muoj.herokuapp.com/', 'localhost']
+
 # Application definition
 INSTALLED_APPS = [
     'jazzmin',
